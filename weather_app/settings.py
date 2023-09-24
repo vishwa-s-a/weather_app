5b84e663190973c387cfe759ae2f08b84d23d984
vishwa-s-a <vishwasappaji769@gmail.com>
2023-09-24 14:08:56 +0530

    editing env file

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -15,6 +15,7 @@ import corsheaders
 from decouple import config
 
 API_KEY = config('API_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hunp(%-zj&t+y3gz5yanp()8+b4o=1*zil(vg7uo*!i*x6%@!^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
